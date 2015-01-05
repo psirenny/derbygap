9567089de65d031823e9febc04a4a161a1d8addb
psirenny <djtorres0@gmail.com>
2015-01-04 19:22:48 -0500

    major update to work with derby 0.6

--- README.md
+++ README.md
@@ -1,4 +1,81 @@
 derbygap
 ========
 
-A simple tool to setup a [PhoneGap](http://phonegap.com) project with [Derby](http://derbyjs.com).
+A simple tool to create a [PhoneGap](http://phonegap.com) app with [Derby](http://derbyjs.com).
+
+Installation
+------------
+
+    $ npm install derbygap --save
+
+Client Usage
+------------
+
+Creating a derbygap app is similar to creating a standard derby app.  
+In addition to creating the app's script bundle, you must also create
+the app's html by calling `derbygap.writeHtml`:
+
+This example assumes you have initialized a phonegap app (named phonegap)
+in the root of your project.  
+
+Create and run the following file:
+
+    var derbyApp = require('…');
+    var expressApp = require('…');
+    var store = require('…');
+    var derby = require('derby');
+    var derbygap = require('derbygap');
+    var path = require('path');
+
+    derby.run(function () {
+      var dir = path.join(__dirname, 'phonegap/www');
+      var localUrl = 'http://localhost:3000/phonegap';
+      var opts = {serverUrl: 'http://foo.bar.com'};
+      expressApp.listen(3000);
+
+      // will write your app bundle to "phonegap/www/derby/….js"
+      derbygap.writeScripts(derbyApp, store, dir, opts, function () {
+        // will write your app html to "phonegap/www/index.html"
+        derbygap.writeHtml(dir, localUrl);
+      });
+    });
+
+Derbygap generates the html by making a request to the running app.  
+You may change the url that derbygap makes the request to.
+Ensure that the route handling the url does not have redirects and DOES NOT render differently for different requests. For instance, it should not setup and return a unique user id per request. Any redirect logic on the route should be run client side only.
+
+Server Usage (optional)
+-----------------------
+
+You may optionally include the derbygap middleware in your express app
+to add `phonegap` data to your model.
+
+    var derbygap = require('derbygap');
+
+    expressApp
+      ...
+      .use(store.modelMiddleware())
+      .use(derbygap.middleware())
+      ...
+
+Middleware
+----------
+
+Adds the following model data:
+
+    **$phonegap.baseUrl** - Blank if phonegap is enabled and `"\"` otherwise.
+
+    **$phonegap.enabled** - `True` if phonegap is enabled.
+
+Options:
+
+    **enabled** - Manually enable/disable this middleware by setting this to something truthy/falsy.
+    **env** - Environment variable that will enable this middleware. Defaults to `PHONEGAP`.
+    **reqHeader** - Request header that will enable this middleware. Defaults to `X-PHONEGAP`.
+
+Notes
+-----
+
+* Make sure the url you generate the html from does not contain server side specific logic, redirects, or change html/data based upon session information.
+
+* Linked files should use relative rather than absolute urls. You can use `$phonegap.baseUrl` in your views as a url prefix for linking to files.
--- index.js
+++ index.js
@@ -1,1 +1,96 @@
-module.exports = require('./lib');
+var derby = require('derby');
+var fs = require('fs');
+var locationify = require('locationify');
+var path = require('path');
+var replace = require('replacestream');
+var request = require('superagent');
+var through = require('through2');
+
+exports.middleware = function (options) {
+  if (!options) options = {};
+  if (!options.env) options.env = 'PHONEGAP';
+  if (!options.reqHeader) options.reqHeader = 'X-PHONEGAP';
+
+  return function(req, res, next) {
+    var enabled = options.enabled;
+    var model = req.getModel();
+    if (enabled == null) enabled = process.env[options.env] || req.get(options.reqHeader);
+    model.set('$phonegap.baseUrl', enabled ? '' : '/');
+    model.set('$phonegap.enabled', enabled);
+    next();
+  };
+};
+
+exports.writeHtml = function (dir, url, callback) {
+  var output = fs.createWriteStream(
+    path.join(dir, 'index.html')
+  );
+
+  request
+    .get(url)
+    .set('X-PHONEGAP', true)
+    .pipe(replace('<script async src="/derby', '<script async src="derby'))
+    .pipe(output)
+    .on('error', callback)
+    .on('finish', callback);
+};
+
+// Code adapted from "derby/lib/App.sever.js"
+exports.writeScripts = function (app, store, dir, options, cb) {
+  store.on('bundle', function (bundle) {
+    // Replace window.location.hostname, window.location.origin, etc...
+    // with the server url so that the phonegap app will connect
+    // to the server rather than the local filesystem.
+    bundle.transform({global: true}, locationify(options.serverUrl));
+
+    // Convert absolute urls to relative urls so that
+    // phonegap does not prepend "file://..." junk to the
+    // beginning of those urls.
+    bundle.transform({global: true}, function (file) {
+      return through(function (buf, enc, next) {
+        var code = buf.toString('utf8');
+        code = code.replace('"/derby/', '"derby/');
+        this.push(code);
+        next();
+      });
+    });
+  });
+
+  app.bundle(store, options, function (err, source, map) {
+    if (err) return cb(err);
+    dir = path.join(dir, 'derby');
+    if (!fs.existsSync(dir)) fs.mkdirSync(dir);
+    var filename = app.name + '-' + app.scriptHash;
+    var base = path.join(dir, filename);
+    app.scriptUrl = '/derby/' + filename + '.js';
+
+    // Write current map and bundle files
+    if (!(options && options.disableScriptMap)) {
+      // Make source map url relative rather than absolute
+      // because phonegap prepends absolute urls with "file://.../" junk.
+      app.scriptMapUrl = filename + '.map.json';
+      source += '\n//# sourceMappingURL=' + app.scriptMapUrl;
+      app.scriptMapFilename = base + '.map.json';
+      fs.writeFileSync(app.scriptMapFilename, map, 'utf8');
+    }
+    app.scriptFilename = base + '.js';
+    fs.writeFileSync(app.scriptFilename, source, 'utf8');
+
+    // Delete app bundles with same name in development so files don't
+    // accumulate. Don't do this automatically in production, since there could
+    // be race conditions with multiple processes intentionally running
+    // different versions of the app in parallel out of the same directory,
+    // such as during a rolling restart.
+    if (!derby.util.isProduction) {
+      var filenames = fs.readdirSync(dir);
+      for (var i = 0; i < filenames.length; i++) {
+        var item = filenames[i].split(/[-.]/);
+        if (item[0] === app.name && item[1] !== app.scriptHash) {
+          var oldFilename = path.join(dir, filenames[i]);
+          fs.unlinkSync(oldFilename);
+        }
+      }
+    }
+    cb();
+  });
+};
